a47b7d00b1a3ba2298c2cf45394fa9fa8729f1cd
NadiaHirwa <irahirnad22@gmail.com>
2025-11-11 15:11:04 +0300

    added migrations

--- backend/environment/admin.py
+++ backend/environment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import District, EnvironmentalData
+
+admin.site.register(District)
+admin.site.register(EnvironmentalData)
 
 # Register your models here.
